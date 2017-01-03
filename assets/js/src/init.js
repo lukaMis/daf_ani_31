5f0f4807acc051de7597accdecb6e7feffeb73d4
Luka Mis <luka.mis@gmail.com>
2017-01-03 15:24:49 +0100

    Mostly work on css for card animation….

--- assets/js/src/init.js
+++ assets/js/src/init.js
@@ -9,6 +9,14 @@
   function oni18nComplete(e) {
     FastClick.attach(document.body);
     $('body').removeClass('loading');
+
+    $('.card').on('click', function() {
+      if($(this).hasClass('active')){
+        $(this).removeClass('active');
+      } else {
+        $(this).addClass('active');
+      }
+    });
   };
 
 
